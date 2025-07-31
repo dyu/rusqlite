68d8e25653f1582f2df73da48ceaade5d3970340
dyu <david.yu.ftw@gmail.com>
2025-07-31 16:04:52 +0800

    bench inserts

---
+++ examples/bench_inserts_exec/src/main.rs
@@ -0,0 +1,91 @@
+extern crate rusqlite;
+use rusqlite::{Connection, Result};
+use std::ffi::{CStr};
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
+use itoa::{Buffer};
+
+const DDL: &'static CStr = c"
+PRAGMA journal_mode=WAL;
+PRAGMA synchronous=OFF;
+PRAGMA page_size=4096;
+PRAGMA journal_size_limit=16384;
+PRAGMA wal_autocheckpoint=4000;
+PRAGMA auto_vacuum=INCREMENTAL;
+PRAGMA foreign_keys=ON;
+PRAGMA optimize=0x10002;
+CREATE TABLE IF NOT EXISTS bench_simple (
+    id INTEGER NOT NULL CONSTRAINT bench_simple_pkey PRIMARY KEY,
+    ts INTEGER NOT NULL,
+    name TEXT    NOT NULL
+);
+";
+
+fn arg_as_num<T: std::str::FromStr>(nth: usize, def: T) -> T {
+    std::env::args().nth(nth)
+        .and_then(|e| e.parse::<T>().ok())
+        .unwrap_or(def)
+}
+
+fn env_as_num<T: std::str::FromStr>(key: &str, def: T) -> T {
+    std::env::var(key)
+        .ok()
+        .and_then(|e| e.parse::<T>().ok())
+        .unwrap_or(def)
+}
+
+fn get_epoch_seconds() -> i64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs() as i64
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = std::fs::remove_file("target/db.sqlite");
+    let _ = std::fs::remove_file("target/db.sqlite-shm");
+    let _ = std::fs::remove_file("target/db.sqlite-wal");
+    
+    let flags: u8 = arg_as_num(1, 0);
+    let begin_end = 0 != (flags & 1);
+    
+    let iterations: usize = env_as_num("ITERATIONS", 5000);
+    
+    let conn = Connection::open("target/db.sqlite")?;
+    conn.exec(DDL)?;
+    
+    let mut buf_id = Buffer::new();
+    let mut buf_ts = Buffer::new();
+    let mut buf = String::new();
+    
+    let start = Instant::now();
+    
+    if begin_end {
+        buf.push_str("BEGIN;")
+    }
+    buf.push_str("INSERT INTO bench_simple (id, ts, name) VALUES (");
+    for idx in 0..iterations {
+        let id = buf_id.format(idx);
+        let ts = buf_ts.format(get_epoch_seconds());
+        if idx != 0 {
+            buf.push_str(",(");
+        }
+        buf.push_str(id);
+        buf.push(',');
+        buf.push_str(ts);
+        buf.push_str(",'");
+        buf.push_str(id);
+        buf.push_str("')");
+    }
+    if begin_end {
+        buf.push_str(";END");
+    }
+    buf.push_str(";\0");
+    
+    conn.exec_ptr(buf.as_ptr())?;
+    
+    let elapsed = start.elapsed().as_millis();
+    let ops_per_sec = (iterations * 1000) / elapsed as usize;
+    eprintln!("begin_end: {begin_end} | iterations: {iterations} | elapsed time: {elapsed} ms | ops/sec: {ops_per_sec}");
+    
+    Ok(())
+}
---
+++ hyperfine.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#set -e
+
+CURRENT_DIR=$PWD
+# locate
+if [ -z "$BASH_SOURCE" ]; then
+    SCRIPT_DIR=`dirname "$(readlink -f $0)"`
+elif [ -e '/bin/zsh' ]; then
+    F=`/bin/zsh -c "print -lr -- $BASH_SOURCE(:A)"`
+    SCRIPT_DIR=`dirname $F`
+elif [ -e '/usr/bin/realpath' ]; then
+    F=`/usr/bin/realpath $BASH_SOURCE`
+    SCRIPT_DIR=`dirname $F`
+else
+    F=$BASH_SOURCE
+    while [ -h "$F" ]; do F="$(readlink $F)"; done
+    SCRIPT_DIR=`dirname $F`
+fi
+# change pwd
+cd $SCRIPT_DIR
+
+TARGETS='
+bench_inserts
+bench_inserts_exec
+'
+
+for F in $TARGETS; do
+    if [ "$BUILD" = '1' ] || [ ! -e "target/release/$F" ]; then
+        cargo build --release --package $F
+    fi
+    du -sh "target/release/$F"
+done
+
+BENCH=1 hyperfine \
+--prepare 'rm -f target/db.sqlite*' \
+--sort command -w 0 -r 1 \
+'./target/release/bench_inserts 0' \
+'./target/release/bench_inserts 1' \
+'./target/release/bench_inserts_exec 0' \
+'./target/release/bench_inserts_exec 1' && \
+echo
